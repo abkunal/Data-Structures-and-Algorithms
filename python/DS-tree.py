d0551ddd05e1bd524331c8b8f65265b6a6e32c3d
Kunal Yadav <abkunal@gmail.com>
2016-12-27 18:12:07 +0530

    Binary Search Tree Added

--- python/DS-tree.py
+++ python/DS-tree.py
@@ -4,81 +4,174 @@ class Node( object ):
 	""" A node contains some information """
 	
 	def __init__( self, value ):
-		self._value = value
-		self._parent = None
-		self._left = None
-		self._right = None
-		
-	def get_value( self ):
-		return self._value
-	
-	def get_parent( self ):
-		return self._parent
-		
-	def get_left( self ):
-		return self._left
-		
-	def get_right( self ):
-		return self._right
-	
-	def set_parent( self, parent ):
-		self._parent = parent
-	
-	def set_left( self, left ):
-		self._left = left
-		
-	def set_right( self, right ):
-		self._right = right
+		self.value = value
+		self.parent = None
+		self.left = None
+		self.right = None
 
 
 def inorder( root ):
 	""" Prints the inorder traversal of a tree """
 	if root != None:
-		inorder( root.get_left() )
-		print( root.get_value(), end=" " )
-		inorder( root.get_right() )
+		inorder( root.left )
+		print( root.value, end=" " )
+		inorder( root.right )
 
 def preorder( root ):
 	""" Prints the preorder traversal of a tree """
 	if root != None:
-		print( root.get_value(), end=" " )
-		preorder( root.get_left() )
-		preorder( root.get_right() )
+		print( root.value, end=" " )
+		preorder( root.left )
+		preorder( root.right )
 
 def postorder( root ):
 	""" Prints the postorder traversal of a tree """
 	if root != None:
-		postorder( root.get_left() )
-		postorder( root.get_right() )
-		print( root.get_value(), end=" " )
+		postorder( root.left )
+		postorder( root.right )
+		print( root.value, end=" " )
 		
 
-def main():
-	node1 = Node( 10 )
-	node2 = Node( 5 )
-	node3 = Node( 2 )
-	node4 = Node( 8 )
-	node5 = Node( 20 )
-	node6 = Node( 15 )
-	node1.set_left( node2 )
-	node1.set_right( node5 )
-	node2.set_parent( node1 )
-	node2.set_left( node3 )
-	node2.set_right( node4 )
-	node3.set_parent( node2 )
-	node4.set_parent( node2 )
-	node5.set_parent( node1 )
-	node5.set_left( node6 )
-	node6.set_parent( node5 )
+
+class BinarySearchTree( object ):
+	""" 
+		Binary search tree is a binary tree in which each node > left child and 
+		<= right child
+	"""
+	def __init__( self ):
+		self.root = None
+		self.length = 0
 	
-	print( "Inorder: ", end=" " )
-	inorder( node1 )
-	print()
-	print( "Preorder: ", end=" " )
-	preorder( node1 )
-	print()
-	print( "Postorder: ", end=" " )
-	postorder( node1 )
-	print()
+	def length( self ):
+		return self._length
+
+
+def TreeSearch( x, key ):
+	""" Search the given node in a Binary Search Tree """
+	if x is None or key == x.value:
+		return x
+	elif x.value < key:
+		return TreeSearch( x.right, key )
+	else:
+		return TreeSearch( x.left, key )
+
+def TreeMinimum( x ):
+	""" Finds the minimum element in a binary search tree """
+	while x.left is not None:
+		x = x.left
+	return x
 	
-main()
+def TreeMaximum( x ):
+	""" Finds the maximum element in a binary search tree """
+	while x.right is not None:
+		x = x.right
+	return x
+	
+def TreeSuccessor( x ):
+	""" Finds the successor of x in a binary Search Tree """
+	if x.right is not None:
+		return TreeMinimum( x )
+	
+	y = x.parent
+	while ( y is not None and x == y.right ):
+		x = y
+		y = y.parent
+	return y
+	
+def TreePredecessor( x ):
+	""" Find the predecessor of x in a binary search tree """
+	if x.left is not None:
+		return TreeMaximum( x )
+		
+	y = x.parent
+	while ( y is not None and x == y.right ):
+		x = y
+		y = y.parent
+	return y
+
+def TreeInsert( T, z ):
+	""" Inserts node x in a binary search tree T """
+	y = None
+	x = T.root
+	# find the right position to insert node z
+	while x is not None:
+		y = x
+		if z.value < x.value:
+			x = x.left
+		else:
+			x = x.right
+	# insert node z in BST T
+	z.parent = y 
+	if y is None:
+		T.root = z		# tree is empty
+	elif z.value < y.value:
+		y.left = z 
+	else: y.right = z 
+
+def Transplant( T, u, v ):
+	""" Replaces node v with node u """
+	if u.parent is None:
+		T.root = v 
+	elif u == u.parent.left: # if u is left node of its parent
+		u.parent.left = v 
+	else:
+		u.parent.right = v 
+	
+	if v is not None:
+		v.parent =  u.parent
+
+def TreeDelete( T, z ):
+	""" Deletes a node z from a BST T """
+	# if left subtree of z is empty replace z with its right child
+	if z.left is None:
+		Transplant( T, z, z.right )
+	# if right subtree of z is empty replace z with its left child
+	elif z.right is None:
+		Transplant( T, z, z.left )
+	else:
+		# find the successor
+		y = TreeMinimum( z.right )
+		# if its not the right child of z
+		if y.parent != z:
+			# replace y with its right child, y has no left child being successor
+			Transplant( T, y, y.right )
+			# connect y with the right subtree z
+			y.right = z.right 
+			y.right.parent =  y
+		# replace z with y
+		Transplant( T, z, y )
+		# connect y with the left subtree of z
+		y.left = z.left 
+		y.left.parent = y
+
+
+
+
+node1 = Node( 10 )
+node2 = Node( 5 )
+node3 = Node( 2 )
+node4 = Node( 8 )
+node5 = Node( 20 )
+node6 = Node( 15 )
+node1.left = node2 
+node1.right = node5 
+node2.parent = node1 
+node2.left = node3 
+node2.right = node4 
+node3.parent = node2 
+node4.parent = node2 
+node5.parent = node1 
+node5.left = node6 
+node6.parent = node5 
+
+print( "Inorder: ", end=" " )
+inorder( node1 )
+print()
+print( "Preorder: ", end=" " )
+preorder( node1 )
+print()
+print( "Postorder: ", end=" " )
+postorder( node1 )
+print()
+	
+
