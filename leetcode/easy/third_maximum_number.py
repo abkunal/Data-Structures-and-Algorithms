0751b5c4ebe91bd360ad0bb0761e14b447bc4851
Kunal Yadav <abkunal@gmail.com>
2017-08-19 11:18:24 +0530

    Third maximum number

---
+++ leetcode/easy/third_maximum_number.py
@@ -0,0 +1,47 @@
+""" Third Maximum Number - https://leetcode.com/problems/third-maximum-number
+
+    Given a non-empty array of integers, return the third maximum number 
+    in this array. If it does not exist, return the maximum number. 
+    The time complexity must be in O(n).
+
+    Example 1:
+    Input: [3, 2, 1]
+
+    Output: 1
+
+    Explanation: The third maximum is 1.
+    Example 2:
+    Input: [1, 2]
+
+    Output: 2
+
+    Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+    Example 3:
+    Input: [2, 2, 3, 1]
+
+    Output: 1
+
+    Explanation: Note that the third maximum here means the third maximum distinct number.
+    Both numbers with value 2 are both considered as second maximum.
+"""
+
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0: return 0
+        nums = set(nums)
+        first = max(nums)
+        nums.remove(first)
+        if nums:
+            second = max(nums)
+        else:
+            return first
+        nums.remove(second)
+        if nums:
+            third = max(nums)
+        else:
+            return first
+        return third
