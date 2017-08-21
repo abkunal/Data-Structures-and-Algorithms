f83d0b75f8b580c9e62b35f75b3391f690b90134
Kunal Yadav <abkunal@gmail.com>
2017-08-22 03:43:50 +0530

    Minimum Moves to Equal Array Elements

---
+++ leetcode/easy/minimum_moves.py
@@ -0,0 +1,30 @@
+""" Minimum Moves to Equal Array Elements
+
+    Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+
+    Example:
+
+    Input:
+    [1,2,3]
+
+    Output:
+    3
+
+    Explanation:
+    Only three moves are needed (remember each move increments two elements):
+
+    [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+"""
+
+class Solution(object):
+    def minMoves(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # subtract the minimum number from all the other numbers and add result
+        mini = min(nums)
+        total = 0
+        for n in nums:
+            total += (n - mini)
+        return total
