a6e3b93ead3aedf178bb4dc87306219b5f3c6a0f
Kunal Yadav <abkunal@gmail.com>
2017-08-29 22:10:57 +0530

    Reverse String II

---
+++ leetcode/easy/reverse_string2.py
@@ -0,0 +1,24 @@
+""" Reverse String - https://leetcode.com/problems/reverse-string-ii
+
+    Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+    
+    Example:
+    Input: s = "abcdefg", k = 2
+    Output: "bacdfeg"
+    
+    Restrictions:
+    The string consists of lower English letters only.
+    Length of the given string and k will in the range [1, 10000]
+"""
+
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        l = list(s)
+        for i in range(0,len(s), 2*k):
+            l[i:i+k] = l[i:i+k][::-1]
+        return "".join(l)
