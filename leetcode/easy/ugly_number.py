81e379794a44632964b2c502e3af782e820cb221
Kunal Yadav <abkunal@gmail.com>
2017-10-11 06:45:10 +0530

    Ugly Number

---
+++ leetcode/easy/ugly_number.py
@@ -0,0 +1,29 @@
+""" Ugly Number - https://leetcode.com/problems/ugly-number
+
+    Write a program to check whether a given number is an ugly number.
+
+    Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+    For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+    Note that 1 is typically treated as an ugly number.
+"""
+
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num < 1:
+            return False
+        while num % 2 == 0:
+            num = num // 2
+        while num % 3 == 0:
+            num = num // 3
+        while num % 5 == 0:
+            num = num // 5
+        return num == 1
+
+
+# a = Solution()
+# print(a.isUgly(10))
