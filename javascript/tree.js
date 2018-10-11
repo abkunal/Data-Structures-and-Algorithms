2c4e53b0d1a61b6256f4c58983c605f32b9a919d
Kunal Yadav <kunal.yadav@myshikshak.com>
2018-10-11 12:49:28 +0530

    Added preorder traversal of tree

--- javascript/tree.js
+++ javascript/tree.js
@@ -35,7 +35,39 @@ class Node {
  * Represents Tree as a collection of nodes
  */
 class Tree {
-  setRoot(value) {
-    this.root = new Node(value);
+  setRoot(node) {
+    this.root = node;
+  }
+
+  /**
+   * Print the preorder traversal of the tree
+   */
+  printPreorderTraversal() {
+    this.preorderHelper(this.root);
+  }
+
+  preorderHelper(node) {
+    if (node !== undefined) {
+      this.preorderHelper(node.left);
+      console.log(node.value);
+      this.preorderHelper(node.right);
+    }
   }
 }
+
+let tree = new Tree();
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+let node5 = new Node(5);
+let node6 = new Node(6);
+
+node1.setLeftChild(node2);
+node1.setRightChild(node3);
+node2.setLeftChild(node4);
+node2.setRightChild(node5);
+node3.setLeftChild(node6);
+tree.setRoot(node1);
+
+tree.printPreorderTraversal();
